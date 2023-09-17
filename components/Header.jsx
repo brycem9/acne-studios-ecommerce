fdab992893d22d47e9e2a65749e22d5f2b8e053b
brycem9 <123997306+brycem9@users.noreply.github.com>
2023-09-17 02:43:55 -0400

    landing page responsive layout

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,35 +13,38 @@ function Header() {
 
   return (
     <div>
-      <div className="">
-        {hovered ? (
-          <h1 className="text-xs sticky pl-3 z-10 left-0 top-10">
+      <div
+        className="img-container-outer mt-8 "
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <div className="img-container">
+          {hovered ? (
+          <h1 className="text-xs sticky pt-2 pb-2 pl-3 z-10 left-0 top-0">
             KYLIE JENNER FW23 DENIM {">"} SHOP NOW
           </h1>
         ) : (
-          <h1 className="text-xs sticky pl-3 z-10 left-0 top-10 max-w-[20%] ">
+          <h1 className="text-xs sticky pt-2 pb-2 pl-3 z-10 left-0 top-0 ">
             KYLIE JENNER FW23 DENIM
           </h1>
         )}{" "}
-
-        <img
-          className="w-full cursor-pointer  h-full pt-3"
-          src={"/assets/kylie/denim-d.jpg"}
-          alt="Kylie Jenner FW23 denim"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        />
+        </div>
+        
       </div>
-      <div className="">
-      <h1 className="text-xs sticky pl-3 z-10 left-0 top-10 max-w-[20%] ">
-            KYLIE JENNER FW23 DENIM
+      <div >
+       <div className="img-container2">
+          {hovered ? (
+          <h1 className="text-xs sticky pt-2  pl-3 z-10 left-0 top-11">
+            WOMEN'S NEW ARRIVALS {">"} SHOP NOW
           </h1>
-        <img
-          className="w-full h-full "
-          src={"/assets/kylie/denim2-d.jpg"}
-          alt=""
-        />
+        ) : (
+          <h1 className="text-xs sticky pt-2 pl-3 z-10 left-0 top-11 ">
+            WOMEN'S NEW ARRIVALS
+          </h1>
+        )}{" "}
+        </div> 
       </div>
+      
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
     <main>
       <Navbar />
       <div className="flex justify-center">
-        {/* <img
-          className="acne-splash w-[75%] fixed top-[50%] left-[50%] mix-blend-luminosity"
+        <img
+          className="acne-splash w-[75%] z-30 fixed top-[50%] left-[50%] mix-blend-luminosity"
           src="/assets/logos/denim-logo.png"
-        /> */}
+        />
         
       </div>
       <Header />
