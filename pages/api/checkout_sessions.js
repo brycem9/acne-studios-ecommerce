93a1d0dbf6c2d6250a977335e0e5a738241b4c05
brycem9 <123997306+brycem9@users.noreply.github.com>
2024-03-31 00:22:32 -0400

    fixed deployed stripe reroute

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
           },
         ],
         mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `https://acne-studios-ecommerce.vercel.app/?success=true`,
+        cancel_url: `https://acne-studios-ecommerce.vercel.app/?canceled=true`,
       });
       res.redirect(303, session.url);
     } catch (err) {
