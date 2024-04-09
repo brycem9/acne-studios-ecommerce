fd3fc9f24ad22347b8267a4a9d20ef18833e2192
brycem9 <123997306+brycem9@users.noreply.github.com>
2024-04-09 12:18:47 -0400

    favicon updatev2

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <link rel="icon" href="public/assets/favicon.ico" sizes="any" />
       <Head />
       <body>
         <Main />
