5970711603fc1a17921a30c2cdfd6a84a4368746
brycem9 <123997306+brycem9@users.noreply.github.com>
2024-04-09 13:52:33 -0400

    favicon fixed

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <link rel="icon" href="public/assets/favicon.ico" sizes="any" />
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="shortcut icon" href="/acnecon.png" type="image/x-icon" />
+          <link rel="icon" href="/acnecon.png" type="image/x-icon" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
